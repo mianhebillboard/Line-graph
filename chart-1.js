43cd0f1163dd2438b3408f27c2f14a94f3d84a17
bobgolosinda <123371037+mianhebillboard@users.noreply.github.com>
2024-06-06 00:03:28 +0800

    Update title and x and y titles

--- chart-1.js
+++ chart-1.js
@@ -8,11 +8,19 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     },
     series: [{
-      name: 'Sales (pesos)',
-      data: [30, 40, 35, 50, 49, 60, 70, 91, 125, 102, 89, 21]  // Modify this array with your values
+      name: 'Reservations Overview',
+      data: [300, 460, 354, 506, 449, 620, 780, 991, 12455, 1042, 849, 2130]  // Modify this array with your values
     }],
     xaxis: {
-      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']  // Modify this array with your categories
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],  // Modify this array with your categories
+      title: {
+        text: 'Months'
+      }
+    },
+    yaxis: {
+      title: {
+        text: 'Reservations'
+      }
     }
   };
 
